1ae9bd84b088b52e8fa77c695de0c9987848b032
Sarah Loos <sarahloos@Sarahs-MacBook-Pro.local>
2019-12-10 16:09:44 +0100

    refactoring variable names and btn names

--- Assets/MyGame/Script/ComputeNumbers.cs
+++ Assets/MyGame/Script/ComputeNumbers.cs
@@ -2,39 +2,38 @@
 using UnityEngine.UI;
 using TMPro;
 
-    
 
 
 public class ComputeNumbers : MonoBehaviour
 { 
     public TextMeshProUGUI result;
-    public TMP_InputField varA;
-    public TMP_InputField varB;
-    public Button btn_reset;
-    public Button btn_sub_Numbers;
+    public TMP_InputField firstNumber;
+    public TMP_InputField secondNumber;
+    public Button btnReset;
+    public Button btnResult;
 
 public void SetResult()
 {
     result.text = AddNumbers().ToString();
-    varA.interactable = false;
-    varB.interactable = false;
-    btn_sub_Numbers.interactable = false;
-    btn_reset.interactable = true;
+    firstNumber.interactable = false;
+    secondNumber.interactable = false;
+    btnResult.interactable = false;
+    btnReset.interactable = true;
 }
 
 public void Reset()
 {
-    varA.text = "0";
-    varB.text = "0";
-    varA.interactable = true;
-    varB.interactable = true;
-    btn_sub_Numbers.interactable = true;
-    btn_reset.interactable = false;
+    firstNumber.text = "0";
+    secondNumber.text = "0";
+    firstNumber.interactable = true;
+    secondNumber.interactable = true;
+    btnResult.interactable = true;
+    btnReset.interactable = false;
     result.text = "Result";
 }
 private float AddNumbers()
 {
-    float tempResult = float.Parse(varA.text) - float.Parse(varB.text);
+    float tempResult = float.Parse(firstNumber.text) - float.Parse(secondNumber.text);
     return tempResult;
 }
 }
